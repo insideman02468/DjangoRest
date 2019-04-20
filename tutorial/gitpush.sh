608000ef688927439083d43f07fd4382076c6a2f
insideman02468 <insideman02468@gmail.com>
2019-04-20 19:57:00 +0900

    Start quickstart Django REST framework tutorial2

--- tutorial/gitpush.sh
+++ tutorial/gitpush.sh
@@ -4,7 +4,7 @@ git config --global user.name "insideman02468"
 git config --global user.email "insideman02468@gmail.com"
 git remote add origin https://github.com/insideman02468/DjangoRest.git
 git status
-git checkout tutorial1
+git checkout tutorial2
 git add --all
-git commit -m "Finish quickstart Django REST framework tutorial1"
-git push origin tutorial1
+git commit -m "Start quickstart Django REST framework tutorial2"
+git push origin tutorial2
